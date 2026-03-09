483fa89271f53b68ef5541e4b39a1e218d237125
shakibparsa <ahmadshakibparsa@gmail.com>
2026-03-09 09:19:43 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -278,5 +278,21 @@ Signals:
 
     time.sleep(SCAN_INTERVAL)
 
+    from flask import Flask
+    import threading
 
-   
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "Crypto Analyzer running"
+
+    def run_web():
+        app.run(host="0.0.0.0", port=10000)
+
+    threading.Thread(target=run_web).start()
+
+
+
+
+
